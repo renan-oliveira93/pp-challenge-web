a5ccf6a3ef4e44c59d7fe96001f14d293738b279
Renan de Oliveira <renan_oliveira93@outlook.com.br>
2022-02-24 11:51:59 -0300

    update

--- pages/cargos.js
+++ pages/cargos.js
@@ -2,6 +2,7 @@ import Table from "../src/components/table/table";
 import Topnav from "../src/components/topnav/topnav";
 import Sidebar from "../src/components/sidebar/sidebar";
 import styles from "../styles/Home.module.css";
+import Head from "next/head";
 
 
 
@@ -9,8 +10,9 @@ export default function Cargospg() {
 
     return (
         <div>
-
-
+            <Head>
+                <title>Pedido Pago | Cargos</title>
+            </Head>
             <Topnav />
             <div className={styles.divprincipal}>
                 <Sidebar />
