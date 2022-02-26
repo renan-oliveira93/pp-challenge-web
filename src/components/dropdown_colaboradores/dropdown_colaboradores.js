271b5e137aca51a91fc697cf57fbe4f059584cdb
Renan de Oliveira <renan_oliveira93@outlook.com.br>
2022-02-26 18:21:47 -0300

    update de responsividade

---
+++ src/components/dropdown_colaboradores/dropdown_colaboradores.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Link from 'next/link';
+import styles from '../../../styles/Home.module.css';
+
+export default function Dropdown_colaboradores() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div>
+            <Button
+                id="basic-button"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+            >
+                <p style={{ color: '#587169' }}>Colaboradores</p> <img src='/assets/menu.png' />
+            </Button>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{ 'aria-labelledby': 'basic-button', }}
+            >
+                <MenuItem style={{ width: '280px', height: '61px' }} onClick={handleClose}>
+                    <Link href='./' ><p style={{ margin: '1rem', color: '#587169' }}>Colaboradores</p></Link></MenuItem>
+                <MenuItem style={{ width: '280px', height: '61px' }} onClick={handleClose}>
+                    <Link href='./cargos' ><p style={{ margin: '1rem', color: '#587169' }}>Cargos</p></Link></MenuItem>
+            </Menu>
+        </div >
+    );
+}
--- src/components/tableColaboradores/table_colaboladores.js
+++ src/components/tableColaboradores/table_colaboladores.js
@@ -3,6 +3,7 @@ import styles from '../../../styles/Home.module.css';
 import Link from "next/link";
 import apiAgents from "../../../pages/api/api_agents";
 import BasicMenu from "../dropdown_menu-detalhes/dropdown_menu-detalhes";
+import Dropdown_colaboradores from "../dropdown_colaboradores/dropdown_colaboradores";
 
 export default function TableColaboradores() {
     const [colaboradores, setColaboradores] = useState(['']);
@@ -20,52 +21,51 @@ export default function TableColaboradores() {
 
 
     return (
-        <div>
-            <div className={styles.table}>
+        <div className={styles.table}>
 
-
-                <div className={styles.nav}>
+            <div className={styles.nav}>
+                <div className={styles.dropdown_colaboradores} ><Dropdown_colaboradores /></div>
+                <div className={styles.colaboradores_desktop}>
                     <Link href='./'><button onChange={handleChange} type="radio" aria-checked='true'>Colaboladores</button></Link>
                     <Link href='./cargos'><button onChange={handleChange} type="radio" aria-checked='false'>Cargos</button></Link>
                 </div>
-
-                <fieldset className={styles.fieldset}>
-                    <legend>Pesquisar por</legend>
-                    <img src="/assets/search.png" width='20px' />
-                    <input className={styles.input} type="search" placeholder="Pesquisar por nome ou cpf" />
-                </fieldset>
-
-
-                <h2>Lista de colaboradores</h2>
-
-                <table className={styles.lista}>
-                    <thead>
-                        <tr>
-                            <th scope="col">Nome Completo</th>
-                            <th scope="col">Departamento</th>
-                            <th scope="col">Cargo</th>
-                            <th scope="col">Unidade</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {colaboradores.items && colaboradores.items.map(coluna => {
-                            return <tr key={coluna.agent_id}>
-
-                                <td className={styles.td}><img height="38" width="38" src={coluna.image} />{coluna.name}</td>
-                                <td>{coluna.department}</td>
-                                <td>{coluna.role}</td>
-                                <td>{coluna.branch}</td>
-                                <td>{coluna.status}</td>
-                                <td><BasicMenu /></td>
-                            </tr>
-                        })}
-
-                    </tbody>
-
-                </table>
             </div>
 
+            <fieldset className={styles.fieldset}>
+                <legend>Pesquisar por</legend>
+                <img src="/assets/search.png" width='20px' />
+                <input className={styles.input} type="search" placeholder="Pesquise por nome ou cpf" />
+            </fieldset>
+
+
+            <h2>Lista de colaboradores</h2>
+
+            <table className={styles.lista}>
+                <thead>
+                    <tr>
+                        <th scope="col">Nome Completo</th>
+                        <th scope="col">Departamento</th>
+                        <th scope="col">Cargo</th>
+                        <th scope="col">Unidade</th>
+                        <th scope="col">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {colaboradores.items && colaboradores.items.map(coluna => {
+                        return <tr key={coluna.agent_id}>
+
+                            <td className={styles.td}><img height="38" width="38" src={coluna.image} />{coluna.name}</td>
+                            <td>{coluna.department}</td>
+                            <td>{coluna.role}</td>
+                            <td>{coluna.branch}</td>
+                            <td>{coluna.status}</td>
+                            <td><BasicMenu /></td>
+                        </tr>
+                    })}
+
+                </tbody>
+
+            </table>
         </div>
 
     )
